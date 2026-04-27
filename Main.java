8707c1f0af8b0c832b5b96990b47eee93b0c2c84
Pragadeeswaran <pragadeeshwaran724@gmail.com>
2026-04-27 20:12:14 +0530

    THis is my first Program written on JAVA

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main{
+    public static void main(String[] args){
+
+        // THIS IS MY FIRST JAVA PROGRAM
+
+        /*
+         THIS
+         IS
+         multi line
+         comment
+        */
+          
+        System.out.println("Om namo narayana!");
+        System.out.println("i like pizza");
+        System.out.println("Its very good");
+        System.out.print("so buy it and make happy of it!");
+
+    }
+
+}
